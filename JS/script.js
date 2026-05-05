f5fdb4850de470842ba8b63d7cc9d76f0dc86209
Bruno Sena <contato.brunosena@gmail.com>
2026-05-05 20:02:28 -0300

    Finalização do Template

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,11 @@
+//Criação do Botão Hamburguer
+
 const mobileBtn = document.querySelector(".btn-mobile");
 const navLinks = document.getElementById("nav-links");
 
 mobileBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show");
 });
+
+
+//
